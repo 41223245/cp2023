1ab225ca8a5f089ea9aab2e7ddaa9d0a2c99e212
41223245 <144990933+41223245@users.noreply.github.com>
2023-12-26 14:26:20 +0000

    練習一

---
+++ downloads/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  
+{
+    // Print Name
+    printf("Name   : Youngson Liu\n"); 
+
+    // Print Date of Birth
+    printf("DOB    : August 25, 2005\n"); 
+
+    // Print Mobile Number
+    printf("Mobile : 07-9999999999\n"); 
+
+    // Indicate successful execution
+    return(0); 
+}
